dc840927bb9251ceb0d314344e694a956c838eb4
Andrew Reynolds <andrew@chambana.net>
2016-02-28 11:53:17 -0600

    basic compile-sass task

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,10 +3,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // Configs
+var config = {
+    'sassFiles': 'src/scss/**/*.scss',
+    'sassMain': 'src/scss/main.scss',
+    'cssDir': 'css/'
+}
 
 // Tasks
 gulp.task('hello', function() {
     console.log('hello world');
 });
 
-gulp.task('default', ['hello']);
+gulp.task('compileSass', function() {
+    return gulp.src(config.sassMain)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(config.cssDir));
+});
+
+gulp.task('default', function() {
+    gulp.watch(config.sassFiles, [compileSass]);
+});
