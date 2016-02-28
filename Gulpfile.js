ac72e0745518411433673d8ed1f5c77a301a91a0
Andrew Reynolds <andrew@chambana.net>
2016-02-28 12:07:53 -0600

    Add nice sass tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,19 @@
 // Dependencies
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 
 // Configs
 var config = {
     'sassFiles': 'src/scss/**/*.scss',
     'sassMain': 'src/scss/main.scss',
-    'cssDir': 'css/'
+    'cssDir': 'css/',
+    'autoprefixerOptions': {
+        browsers: ['last 2 versions', '> 5%']
+    }
 }
 
 // Tasks
@@ -16,7 +23,12 @@ gulp.task('hello', function() {
 
 gulp.task('compileSass', function() {
     return gulp.src(config.sassMain)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(config.autoprefixerOptions.browsers))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write())
+        .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest(config.cssDir));
 });
 
